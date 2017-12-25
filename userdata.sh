76f7e1ffe6a4b810bfda8eef006063821d39a971
Michael Soh <devnull@mikesoh.com>
2017-12-24 19:47:13 -0500

    Added Userdata file so I don't have to keep looking it up

---
+++ userdata.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# This file should be copied into the User Data section of creating AWS
+# EC2 instaces.
+bash <(curl -s https://raw.githubusercontent.com/sohmc/aws-bootstrap/master/bootstrap.sh);
