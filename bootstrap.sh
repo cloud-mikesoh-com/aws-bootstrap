668b47dccd63c1cad5842f0762a67ed785e5d4bc
Michael Soh <devnull@mikesoh.com>
2017-12-24 20:21:55 -0500

    Lots of comments for debugging

--- bootstrap.sh
+++ bootstrap.sh
@@ -14,11 +14,14 @@ MYSQL_VERSION='5.7.20-0ubuntu0.16.04.1'
 
 
 # Update apt
+echo Updating apt repositories
 apt update
 
 # Install all patches as necessary, assuming 'y' to questions, and
 # supressing output
+echo Upgrade all available packages
 DEBIAN_FRONTEND=noninteractive apt upgrade -yq
 
 # Install MySQL
+echo Installing MySQL Server version ${MYSQL_VERSION}
 apt install -y install mysql-server=${MYSQL_VERSION}
--- userdata.sh
+++ userdata.sh
@@ -2,4 +2,5 @@
 
 # This file should be copied into the User Data section of creating AWS
 # EC2 instaces.
+echo Downloading Bootstrap from git@github.com:sohmc/aws-bootstrap/bootstrap.sh
 bash <(curl -s https://raw.githubusercontent.com/sohmc/aws-bootstrap/master/bootstrap.sh);
