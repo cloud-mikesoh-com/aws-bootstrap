063b978983c9df931408dbb443646572a2a263a3
Michael Soh <devnull@mikesoh.com>
2017-12-26 10:17:39 -0500

    Better echoing with spaces

--- packages/mysql.sh
+++ packages/mysql.sh
@@ -5,9 +5,9 @@ MYSQL_VERSION='5.7.20-0ubuntu0.16.04.1'
 
 # Install MySQL
 echo Installing MySQL Server version ${MYSQL_VERSION}
-echo     The password will be set to PASSWORD.  You can change this by
-echo     issuing the following command:
-echo         mysqladmin -u root password newpasswordgoeshere
+echo "    The password will be set to PASSWORD.  You can change this by"
+echo "    issuing the following command:"
+echo "        mysqladmin -u root password newpasswordgoeshere"
 sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password password PASSWORD'
 sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password_again password PASSWORD'
 
