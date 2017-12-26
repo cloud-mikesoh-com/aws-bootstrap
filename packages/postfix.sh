71fd6b8f48b62f2916677bf0ccbb5c1d6153d189
Michael Soh <devnull@mikesoh.com>
2017-12-26 18:43:29 -0500

    Adding the config repository

--- packages/postfix.sh
+++ packages/postfix.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-#### VERSION DECLARATION ####
+#### VARIABLE DECLARATION ####
 POSTFIX_VERSION='3.1.0-3'
+POSTFIX_REPO='git@bitbucket.org:sohmc/postfix-config.git'
 
 
 # Install Postfix
@@ -13,10 +14,14 @@ DEBIAN_FRONTEND=noninteractive apt-get install -yq postfix=${POSTFIX_VERSION} po
 echo Stopping Postfix service
 service postfix stop
 
-#
 echo Moving postfix configuration files from /etc/postfix to /etc/postfix__DEFAULT
 cp -r /etc/postfix /etc/postfix__DEFAULT
 
+# Clone the postfix configuration repository
+echo Cloning the postfix configuration repository
+echo "   from ${POSTFIX_REPO}"
+echo "   into /tmp/postfix-config-repo"
+git clone -q ${POSTFIX_REPO} /tmp/postfix-config-repo
 
 
 # Install support packages
