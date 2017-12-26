39dd0a85f850dcb49256da8e101dbc16b998073c
Michael Soh <devnull@mikesoh.com>
2017-12-26 18:00:15 -0500

    Move the default config files
    I'll be importing my config in a few commits

--- packages/postfix.sh
+++ packages/postfix.sh
@@ -9,6 +9,15 @@ echo Installing Postfix version ${POSTFIX_VERSION}
 
 DEBIAN_FRONTEND=noninteractive apt-get install -yq postfix=${POSTFIX_VERSION} postfix-mysql=${POSTFIX_VERSION}
 
+# Stop Postfix
+echo Stopping Postfix service
+service postfix stop
+
+#
+echo Moving postfix configuration files from /etc/postfix to /etc/postfix__DEFAULT
+cp -r /etc/postfix /etc/postfix__DEFAULT
+
+
 
 # Install support packages
 apt-get install -yq procmail
