6f4c1df3c00f690e52588d830ab5bb6211e538b3
Michael Soh <devnull@mikesoh.com>
2017-12-27 17:14:37 -0500

    Configuration of Postfix
    Added lots of configuration options for Postfix.  This is in tandem with
    the postfix configuration repository.

--- packages/postfix.sh
+++ packages/postfix.sh
@@ -24,4 +24,35 @@ echo "   into /tmp/postfix-config-repo"
 git clone -q ${POSTFIX_REPO} /tmp/postfix-config-repo
 
 # Starting configuration of Postfix
+echo Starting configuration of Postfix
+
+echo "===== main.cf ====="
+echo Setting relayhost to AWS SMTP Server
+sed -ir 's/^(relayhost)\s*=/\1 = [email-smtp.us-east-1.amazonaws.com]:587/' /etc/postfix/main.cf
+
+echo Inserting SMTP Options...
+for t in /tmp/postfix-config-repo/postfix/main.cf-injections/*.cf
+do
+    echo Injecting file $t into main.cf
+    echo "" >> /etc/postfix/main.cf
+    cat $t >> /etc/postfix/main.cf
+done
+
+echo ""
+
+echo Copying files for virtual aliases
+cp -r /tmp/postfix-config-repo/postfix/virtual /etc/postfix/
+
+echo "===== sasl_passwd ====="
+echo Copying sasl_passwd from the repository...
+cp /tmp/postfix-config-repo/postfix/sasl_passwd /etc/postfix/sasl_passwd
+
+echo ""
+
+echo Compiling lookup databases
+echo Compiling SASL Password
+postmap hash:/etc/postfix/sasl_passwd
+
+echo Compiling Virtual Lookups
+postmap /etc/postfix/virtual/*
 
