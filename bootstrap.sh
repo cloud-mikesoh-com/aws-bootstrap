0410792dde85babe1e6ff6589f092cc1917c8eeb
Michael Soh <devnull@mikesoh.com>
2017-12-29 13:48:54 -0500

    Reverted userdata and bootstrap back to download from 0.3.0

--- bootstrap.sh
+++ bootstrap.sh
@@ -10,55 +10,55 @@
 
 
 #### VARIABLE DECLARATION ####
-BOOTSTRAP_VERSION='adding-docker'
+BOOTSTRAP_VERSION='0.3.0'
 NEW_HOSTNAME='husker.mikesoh.com'
 
 # Update apt-get
-#echo Updating apt-get repositories
-#apt-get update -yqq
+echo Updating apt-get repositories
+apt-get update -yqq
 
 # Install all patches as necessary, assuming 'y' to questions, and
 # supressing output
-#echo Upgrade all available packages
-#DEBIAN_FRONTEND=noninteractive apt-get upgrade -yq
+echo Upgrade all available packages
+DEBIAN_FRONTEND=noninteractive apt-get upgrade -yq
 
 # Install pre-requisits
-#echo Installing AWS CLI
-#apt-get install -yqq awscli
+echo Installing AWS CLI
+apt-get install -yqq awscli
 
 
 # Install SSH deployment keys
-#echo Downloading git deployment keys from S3
-#aws s3 cp s3://mikesoh.com-galactica-backup/ssh_keys/bitbucket-deployment-keys/ ~/.ssh/ --recursive --quiet
+echo Downloading git deployment keys from S3
+aws s3 cp s3://mikesoh.com-galactica-backup/ssh_keys/bitbucket-deployment-keys/ ~/.ssh/ --recursive --quiet
 
-#echo `ls -1 ~/.ssh/id_rsa* | wc -l` files downloaded.
-#echo Public key as follows:
-#echo ""
-#cat ~/.ssh/id_rsa.pub
-#echo ""
+echo `ls -1 ~/.ssh/id_rsa* | wc -l` files downloaded.
+echo Public key as follows:
+echo ""
+cat ~/.ssh/id_rsa.pub
+echo ""
 
-#echo Making the private key readable by owner only.
-#chmod 600 ~/.ssh/id_rsa
+echo Making the private key readable by owner only.
+chmod 600 ~/.ssh/id_rsa
 
 
 # Set hostname
 # Instructions on how to do this without restarting:
 # https://www.cyberciti.biz/faq/ubuntu-change-hostname-command/
-#OLD_HOSTNAME=`cat /etc/hostname`
-#echo Current Hostname: ${OLD_HOSTNAME}
-#echo Setting hostname to ${NEW_HOSTNAME}
-#hostname ${NEW_HOSTNAME}
+OLD_HOSTNAME=`cat /etc/hostname`
+echo Current Hostname: ${OLD_HOSTNAME}
+echo Setting hostname to ${NEW_HOSTNAME}
+hostname ${NEW_HOSTNAME}
 
-#echo Modifying /etc/hostname
-#sed -ri 's/'"${OLD_HOSTNAME}"'/'"${NEW_HOSTNAME}"'/g' /etc/hostname
+echo Modifying /etc/hostname
+sed -ri 's/'"${OLD_HOSTNAME}"'/'"${NEW_HOSTNAME}"'/g' /etc/hostname
 
-#echo Modifying /etc/hosts
-#sed -ri 's/^(127.0.0.1)\s+(\w+)$/\1 '"${NEW_HOSTNAME}"'/' /etc/hosts
+echo Modifying /etc/hosts
+sed -ri 's/^(127.0.0.1)\s+(\w+)$/\1 '"${NEW_HOSTNAME}"'/' /etc/hosts
 
 
 # Populate SSH Server Keys
-#echo Getting SSH Host keys for bitbucket and github
-#ssh-keyscan github.com bitbucket.org >> ~/.ssh/known_hosts
+echo Getting SSH Host keys for bitbucket and github
+ssh-keyscan github.com bitbucket.org >> ~/.ssh/known_hosts
 
 
 # Download AWS Bootstrap Files
--- userdata.sh
+++ userdata.sh
@@ -2,7 +2,7 @@
 
 # This file should be copied into the User Data section of creating AWS
 # EC2 instaces.
-BOOTSTRAP_FILE='https://raw.githubusercontent.com/sohmc/aws-bootstrap/adding-docker/bootstrap.sh'
+BOOTSTRAP_FILE='https://raw.githubusercontent.com/sohmc/aws-bootstrap/master/bootstrap.sh'
 
 echo Downloading Bootstrap from ${BOOTSTRAP_FILE}
 bash <(curl -s ${BOOTSTRAP_FILE})
