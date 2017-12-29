096c7f79c20506966d405483f557e0e6e46af3b2
Michael Soh <devnull@mikesoh.com>
2017-12-29 12:59:57 -0500

    added ubuntu to the version number

--- packages/20-docker.sh
+++ packages/20-docker.sh
@@ -2,10 +2,10 @@
 
 #### VARIABLE DECLARATION ####
 UBUNTU_VERSION=xenial
-PACKAGE_VERSION='17.12.0~ce-0'
+PACKAGE_VERSION='17.12.0~ce-0~ubuntu'
 
 # Download the docker installation file
-DOCKER_DL_URL="https://download.docker.com/linux/ubuntu/dists/${UBUNTU_VERSION}/pool/stable/amd64/docker-ce_${PACKAGE_VERSION}~amd64.deb"
+DOCKER_DL_URL="https://download.docker.com/linux/ubuntu/dists/${UBUNTU_VERSION}/pool/stable/amd64/docker-ce_${PACKAGE_VERSION}_amd64.deb"
 echo Downloading docker version ${PACKAGE_VERSION} from
 echo "    ${DOCKER_DL_URL}"
 curl -LkSs ${DOCKER_DL_URL} -o /tmp/docker.deb
