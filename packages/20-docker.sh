44332d3ee1d56bfb302117593e75a96289f4c5ee
Michael Soh <devnull@mikesoh.com>
2017-12-29 12:48:30 -0500

    Variables to make debugging easier

--- packages/20-docker.sh
+++ packages/20-docker.sh
@@ -5,9 +5,10 @@ UBUNTU_VERSION=xenial
 PACKAGE_VERSION='17.12.0~ce-0'
 
 # Download the docker installation file
-echo Downloading docker version ${PACKAGE_VERSION}
-curl -LkSs https://download.docker.com/linux/ubuntu/dists/${UBUNTU_VERSION}/pool/stable/amd64/docker-ce_${PACKAGE_VERSION}~amd64.deb \
-    -o /tmp/docker.deb
+DOCKER_DL_URL='https://download.docker.com/linux/ubuntu/dists/${UBUNTU_VERSION}/pool/stable/amd64/docker-ce_${PACKAGE_VERSION}~amd64.deb'
+echo Downloading docker version ${PACKAGE_VERSION} from
+echo "    ${DOCKER_DL_URL}"
+curl -LkSs ${DOCKER_DL_URL} -o /tmp/docker.deb
 
 echo Installing docker
 dpkg -i /tmp/docker.deb
