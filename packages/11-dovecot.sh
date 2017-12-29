87381bee6baff00d83599d928f08d25d26a07501
Michael Soh <devnull@mikesoh.com>
2017-12-29 12:12:54 -0500

    Added configuration of Dovecot

--- packages/11-dovecot.sh
+++ packages/11-dovecot.sh
@@ -10,7 +10,20 @@ DEBIAN_FRONTEND=noninteractive apt-get install -yqq \
     dovecot-core=${PACKAGE_VERSION} \
     dovecot-mysql=${PACKAGE_VERSION}
 
-# Configure
-echo Do configuration stuff here
 
+echo Stopping dovecot...
+service dovecot stop
 
+echo Copying dovecot configuration files from /etc/dovecot to /etc/dovecot__DEFAULT
+cp -rf /etc/dovecot /etc/dovecot__DEFAULT
+
+# Configure authentication
+echo "===== 10-auth.conf ====="
+echo Disabling plaintext authentication
+sed -ri 's/^#(disable_plaintext_auth = yes)$/\1/' /etc/dovecot/conf.d/10-auth.conf
+
+echo Enabling login authentication
+sed -ri 's/^(auth_mechanisms) =/\1 = plain login cram-md5/' /etc/dovecot/conf.d/10-auth.conf
+
+echo Enabling authentication via passwd file
+sed -ri 's/^#(!include auth-passwdfile.conf.ext)$/\1/' /etc/dovecot/conf.d/10-auth.conf
