807bd0562095efce4265428b4c7fc730629def4c
Michael Soh <devnull@mikesoh.com>
2018-07-16 16:19:33 -0400

    Need to start changing version numbers
    Looks like apt doesn't have the version numbers that I have already put
    into github.

--- userdata.sh
+++ userdata.sh
@@ -2,7 +2,7 @@
 
 # This file should be copied into the User Data section of creating AWS
 # EC2 instances.
-BOOTSTRAP_FILE='https://raw.githubusercontent.com/sohmc/aws-bootstrap/master/bootstrap.sh'
+BOOTSTRAP_FILE='https://raw.githubusercontent.com/sohmc/aws-bootstrap/changing-version-numbers/bootstrap.sh'
 
 echo Downloading Bootstrap from ${BOOTSTRAP_FILE}
 bash <(curl -s ${BOOTSTRAP_FILE})
