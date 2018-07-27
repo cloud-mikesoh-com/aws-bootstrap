4a6ca19feae370e54ffa0907c0d8a02bcccb5e62
Michael Soh <devnull@mikesoh.com>
2018-07-27 08:36:29 -0400

    Made the backup location as a variable for easier parsing

--- packages/00-mysql.sh
+++ packages/00-mysql.sh
@@ -3,6 +3,8 @@
 #### VERSION DECLARATION ####
 MYSQL_VERSION='5.7.*'
 
+BACKUP_LOCATION='s3://mikesoh.com-galactica-backup/mysql-backups/latest/'
+
 # Install MySQL
 echo Installing MySQL Server version ${MYSQL_VERSION}
 echo "    The password will be set to PASSWORD.  You can change this by"
@@ -12,19 +14,22 @@ sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password passwor
 sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password_again password PASSWORD'
 
 DEBIAN_FRONTEND=noninteractive apt-get install -yqq \
-                    mysql-server=${MYSQL_VERSION}
+    mysql-server=${MYSQL_VERSION}
 
 # Download the latest backup from S3 bucket
-echo Download the latest backup from the S3 Bucket
-aws s3 cp s3://mikesoh.com-galactica-backup/mysql-backups/latest/ /tmp/ --recursive --quiet
+echo Downloading the latest backup from the S3 Bucket
+echo     ${BACKUP_LOCATION}
+aws s3 cp ${BACKUP_LOCATION} /tmp/ --recursive --quiet
 
 # Untar the tar file
 echo Untarring the file
 tar xzf /tmp/mysqldump*.tar.gz -C /tmp
 
 # Import the mysqldump
-echo Import the mysqldump file into the database
+echo Importing the mysqldump file into the database
 mysql -u root --password=PASSWORD --force < /tmp/mysqldump.sql
 
-echo Restart MySQL Database
+echo Restarting MySQL Database
 service mysql restart
+
+echo Done.
