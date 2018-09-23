bc3a547606e38207d2c03f0379c94535e444762d
Michael Soh <devnull@mikesoh.com>
2018-09-23 19:11:26 -0400

    Updated script to properly interpolate the variable

--- userdata.sh
+++ userdata.sh
@@ -2,8 +2,8 @@
 
 # This file should be copied into the User Data section of creating AWS
 # EC2 instances.
-BRANCH_NAME='update_docker_install'
-BOOTSTRAP_FILE='https://raw.githubusercontent.com/sohmc/aws-bootstrap/${BRANCH_NAME}/bootstrap.sh'
+BRANCH_NAME="update_docker_install"
+BOOTSTRAP_FILE="https://raw.githubusercontent.com/sohmc/aws-bootstrap/${BRANCH_NAME}/bootstrap.sh"
 
 echo Downloading Bootstrap from ${BOOTSTRAP_FILE}
 bash <(curl -s ${BOOTSTRAP_FILE})
