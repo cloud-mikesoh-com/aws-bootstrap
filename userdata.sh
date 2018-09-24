11c73b49d6513da584ded55393928e125981668e
Michael Soh <devnull@mikesoh.com>
2018-09-23 20:25:15 -0400

    Gotta export the branch name

--- userdata.sh
+++ userdata.sh
@@ -6,6 +6,7 @@ BRANCH_NAME="changing-version-numbers"
 BOOTSTRAP_FILE="https://raw.githubusercontent.com/sohmc/aws-bootstrap/${BRANCH_NAME}/bootstrap.sh"
 
 echo Downloading Bootstrap from ${BOOTSTRAP_FILE}
+export BRANCH_NAME
 bash <(curl -s ${BOOTSTRAP_FILE})
 
 
