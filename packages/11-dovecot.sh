be67953d0ee2ed5052c583eae037dc7107948346
Michael Soh <sohmc@users.noreply.github.com>
2018-10-03 19:56:41 -0400

    Forgot to start dovecot back up...

--- packages/11-dovecot.sh
+++ packages/11-dovecot.sh
@@ -14,7 +14,7 @@ DEBIAN_FRONTEND=noninteractive apt-get install -yqq \
 
 
 echo Stopping dovecot...
-service dovecot stop
+systemctl stop dovecot
 
 echo Copying dovecot configuration files from /etc/dovecot to /etc/dovecot__DEFAULT
 cp -rf /etc/dovecot /etc/dovecot__DEFAULT
@@ -29,3 +29,6 @@ sed -ri 's/^(auth_mechanisms) =/\1 = plain login cram-md5/' /etc/dovecot/conf.d/
 
 echo Enabling authentication via passwd file
 sed -ri 's/^#(!include auth-passwdfile.conf.ext)$/\1/' /etc/dovecot/conf.d/10-auth.conf
+
+echo Starting dovecot...
+systemctl start dovecot
