dd40bcd3c068c970c7ddf0f0bab4d7b200230db9
Michael Soh <sohmc@users.noreply.github.com>
2020-08-29 21:18:00 -0400

    renamed so that things make sense

---
+++ deployment_scripts/launch_spot_windows_instance.bash
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+FILE=./windows.ps
+LAUNCH_CONFIG=./ec2-windowsServer2020-t3-medium.json
+
+run_me () {
+    USERDATA=`cat ${FILE} | base64 -w 0`
+
+    if [ -e tmp.json ]; then
+        rm tmp.json
+    fi
+
+    echo Generating userdata...
+    sed -r 's/\"\$USERDATA\"/\"'"${USERDATA}"'\"/' ./${LAUNCH_CONFIG} > tmp.json
+
+    echo Requesting spot instance...
+    aws ec2 request-spot-instances --spot-price 0.03 --type "one-time" \
+        --launch-specification file://tmp.json
+
+    rm tmp.json
+}
+
+
+if [ -z ${1} ]; then
+    echo "No parameter provided.  Default is ${FILE}".
+elif [ ! -f ${1} ]; then
+    echo "usage: ${0} <filename of userdata script to run at instance creation>"
+    exit 1
+else
+    FILE=${1}
+fi
+
+run_me
