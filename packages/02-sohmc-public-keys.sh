6741f038b12e208250db5fc9a657e5312459db36
Michael Soh <2767724+sohmc@users.noreply.github.com>
2021-11-10 19:31:25 -0500

    updated public keys deployment

--- packages/02-sohmc-public-keys.sh
+++ packages/02-sohmc-public-keys.sh
@@ -9,11 +9,27 @@ echo Downloading publish SSH keys for sohmc
 # Install authorized keys
 echo Downloading authorized_keys from github
 curl -L "https://github.com/sohmc/ssh-keys/releases/download/latest/authorized_keys" \
-    -o "/tmp/authorized_keys" && \
-    rm ~ubuntu/.ssh/authorized_keys && \
-    mv /tmp/authorized_keys ~ubuntu/.ssh/authorized_keys && \
-    chown ubuntu:ubuntu ~ubuntu/.ssh/authorized_keys && \
-    chmod 600 ~ubuntu/.ssh/authorized_keys
+    -o "/tmp/authorized_keys"
+
+if [[ -d ~ubuntu/.ssh ]]; then
+  if [[ -f ~ubuntu/.authorized_keys ]]; then
+    echo Removing existing authorized keys
+    rm ~ubuntu/.ssh/authorized_keys
+  fi
+else
+  echo Making .ssh directory
+  mkdir ~ubuntu/.ssh
+fi
+
+
+echo Moving authorized_keys from tmp to .ssh
+mv /tmp/authorized_keys ~ubuntu/.ssh/authorized_keys
+
+echo chowning to ubuntu
+chown ubuntu:ubuntu ~ubuntu/.ssh/authorized_keys && \
+
+echo chmoding to strict user ownership
+chmod 600 ~ubuntu/.ssh/authorized_keys
 
 echo Authorized keys updated.  Showing public keys installed:
 cat ~ubuntu/.ssh/authorized_keys
